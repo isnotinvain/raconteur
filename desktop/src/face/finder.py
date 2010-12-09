1a40f4cc42d85cb73498fbb52ffd0f59f259af9c
Alex Levenson <alex@isnotinvain.com>
2010-12-09 04:36:53 -0500

    major refactoring of finder, also fixed scale_to_size bug

--- desktop/src/face/finder.py
+++ desktop/src/face/finder.py
@@ -5,17 +5,14 @@
 '''
 
 import sys
-from opencv.cv import cvLoadHaarClassifierCascade, cvCreateImage, cvCvtColor, CV_BGR2GRAY, \
-cvEqualizeHist, cvHaarDetectObjects, cvCreateMemStorage, cvSize, cvPoint, cvRectangle, \
-CV_RGB
+import opencv as cv
 
-from opencv.highgui import cvCreateFileCapture, cvQueryFrame, \
-cvCreateVideoWriter, CV_FOURCC, cvWriteFrame
+import opencv.highgui as hg
 
 import util.image
 
 cascade_path = "haarcascades/haarcascade_frontalface_alt.xml"
-cascade = cvLoadHaarClassifierCascade(cascade_path,(1,1))
+cascade = cv.cvLoadHaarClassifierCascade(cascade_path,(1,1))
 if not cascade:
     raise Exception("Couldn't load classifier file")
 
@@ -29,13 +26,13 @@ class FaceRect(object):
 def find_faces(img,scale_factor=1.85, min_neighbors=3, flags=0, min_size=(30, 30)):    
     
     # create a grayscale copy for the classifier to work on
-    gray = cvCreateImage((img.width,img.height),8,1) 
-    cvCvtColor(img,gray,CV_BGR2GRAY)
+    gray = cv.cvCreateImage((img.width,img.height),8,1) 
+    cv.cvCvtColor(img,gray,cv.CV_BGR2GRAY)
         
-    cvEqualizeHist(gray,gray)
+    #cv.cvEqualizeHist(gray,gray)
     
     # now the face detection
-    faces_raw = cvHaarDetectObjects(gray,cascade,cvCreateMemStorage(),scale_factor, min_neighbors, flags, cvSize(*min_size))
+    faces_raw = cv.cvHaarDetectObjects(gray,cascade,cv.cvCreateMemStorage(),scale_factor, min_neighbors, flags, cv.cvSize(*min_size))
     
     faces = []
     for face in faces_raw:
@@ -47,26 +44,50 @@ def find_faces(img,scale_factor=1.85, min_neighbors=3, flags=0, min_size=(30, 30
 def draw_faces(img,faces):    
     for face_rect in faces:
         # blit rectangles for each face to the color img
-        pt1 = cvPoint(face_rect.x,face_rect.y)
-        pt2 = cvPoint(face_rect.x+face_rect.width,face_rect.y+face_rect.height)
-        cvRectangle(img,pt1,pt2,CV_RGB(0,255,0),8,0)
+        pt1 = cv.cvPoint(face_rect.x,face_rect.y)
+        pt2 = cv.cvPoint(face_rect.x+face_rect.width,face_rect.y+face_rect.height)
+        cv.cvRectangle(img,pt1,pt2,cv.CV_RGB(0,255,0),8,0)
     
     
 if __name__ == "__main__":
-    if not len(sys.argv) == 3: raise Exception("Requires 2 command line arguments, input file path and output file path")
+    if len(sys.argv) < 2: raise Exception("Requires at least one argument: <input file> <optional output file>")
     
-    capture = cvCreateFileCapture(sys.argv[1])
+    capture = hg.cvCreateFileCapture(sys.argv[1])
     if not capture:
         raise Exception("Couldn't load file"+sys.argv[1])
 
-    writer=cvCreateVideoWriter(sys.argv[2],CV_FOURCC('P','I','M','1'),30,cvSize(888,500),True)    
-
-    while True:
-        frame = cvQueryFrame(capture)
-        if frame is None: break
-        s = util.image.scale_to_size(frame,500,500)
+    frame = hg.cvQueryFrame(capture)    
+    if frame is None: sys.exit()
+    
+    s_frame,s_frame_size = util.image.scale_to_size(frame, 500,500)
+       
+    writer = None
+    if len(sys.argv) >= 3:
+        writer=hg.cvCreateVideoWriter(sys.argv[2], \
+                                      hg.CV_FOURCC('P','I','M','1'), #hg.cvGetCaptureProperty(capture,hg.CV_CAP_PROP_FOURCC), \
+                                      hg.cvGetCaptureProperty(capture,hg.CV_CAP_PROP_FPS), \
+                                      cv.cvSize(*s_frame_size), \
+                                      True)
+    else:
+        hg.cvNamedWindow("Hello Face Detection")
+    
+    frame_count = 0
+    total_frames = hg.cvGetCaptureProperty(capture,hg.CV_CAP_PROP_FRAME_COUNT)
+    while frame is not None:                      
+        s_frame,s_frame_size = util.image.scale_to_size(frame, 500,500)
+        faces = find_faces(s_frame,scale_factor=1.1, min_neighbors=3, flags=0, min_size=(10,10))
+        draw_faces(s_frame,faces)
         
-        faces = find_faces(s,scale_factor=1.1, min_neighbors=3, flags=0, min_size=(100, 100))
-        draw_faces(s,faces)
-        cvWriteFrame(writer,frame)
+        if writer:
+            hg.cvWriteFrame(writer,s_frame)
+            print frame_count / total_frames 
+             
+        else:
+            hg.cvShowImage("Hello Face Detection",s_frame)
+            k = hg.cvWaitKey(5)
+            if k == '\x1b': break
+        
+        frame = hg.cvQueryFrame(capture)
+        frame_count += 1
+    hg.cvReleaseCapture(capture)
     print "done!"
--- desktop/src/util/image.py
+++ desktop/src/util/image.py
@@ -7,15 +7,15 @@ from opencv import cvCreateImage,cvResize,CV_INTER_LINEAR
 
 def scale_to_size(img,max_width,max_height):
     """
-    returns a scaled copy of img that fits inside the box created by max_width and max_height    
+    @return a scaled copy of img that fits inside the box created by max_width and max_height, size it was scaled to    
     """
     wfactor,hfactor = 1,1
     
     if img.width > max_width: wfactor = float(max_width)/img.width
     if img.height > max_height: hfactor = float(max_height)/img.height
 
-    factor = max(wfactor,hfactor)
-            
-    scaled = cvCreateImage((int(img.width*factor),int(img.height*factor)),img.depth,img.nChannels)
+    factor = min(wfactor,hfactor)
+    size = (int(img.width*factor),int(img.height*factor))
+    scaled = cvCreateImage(size,img.depth,img.nChannels)
     cvResize(img,scaled,CV_INTER_LINEAR)
-    return scaled    
+    return scaled,size
