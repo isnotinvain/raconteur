cd1c0cae10dfe19fb9504980d478f0fae018558e
Alex Levenson <alex@isnotinvain.com>
2011-01-27 17:46:24 -0500

    messing w/ face tracker

--- desktop/src/face/tracker.py
+++ desktop/src/face/tracker.py
@@ -3,23 +3,48 @@
 
 @author: Alex Levenson (alex@isnotinvain.com)
 '''
-import cv
 
+from util.video import Video
 import face.object_finder
 
 class ObjectTracker(object):
     def __init__(self,video,finder=face.object_finder.HaarFinder()):
         self.video = video
         self.finder = finder
-        
+                
     def get_raw_object_bounds(self):
         bounds = {}
-        for frame in self.video.frames():
-            frame_no,raw = self.finder.find_in_image(frame)
+        for frame_no,frame in self.video.frames():
+            raw = self.finder.find_in_image(frame)
             bounds[frame_no] = raw
         return bounds
     
-    def draw_raw_bounds(self,img,bounds,frame_no,color=(255,0,0)):
-        for (x,y,w,h),_ in bounds[frame_no]:
-            cv.Rectangle(img, (x,y), (x+w,y+h), color)
+    def get_n_raw_object_bounds(self,n=100):
+        i = 0
+        bounds = {}
+        for frame_no,frame in self.video.frames():
+            if i > n: break
+            raw = self.finder.find_in_image(frame)
+            bounds[frame_no] = raw
+            i+=1            
+        return bounds
         
+    
+    def classify_raw_bounds(self,raw_bounds,frame_step=5,similarity=0.90):        
+        frame_nos = sorted(raw_bounds.iterkeys())
+        for f in frame_nos:
+            for (x,y,w,h),_ in raw_bounds[f]:
+                print f, (x,y,w,h)
+                
+        
+    def get_object_bounds(self):
+        pass
+    
+if __name__ == "__main__":
+    video = Video("/home/alex/Desktop/blarg.avi")
+    t = ObjectTracker(video)
+    
+    b = t.get_n_raw_object_bounds()
+    
+    t.classify_raw_bounds(b)
+    
