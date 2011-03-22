901780f9af3691b53ebda12e53f301e11958e192
Alex Levenson <alex@isnotinvain.com>
2011-03-22 15:27:37 -0400

    gonna abandon sizers...

--- desktop/src/gui/widgets/videoScrollPanel.py
+++ desktop/src/gui/widgets/videoScrollPanel.py
@@ -4,20 +4,30 @@ import cv
 
 class VideoScrollPanel(wx.ScrolledWindow):
     
-    def __init__(self,parent,orientation,**kwargs):
+    def __init__(self,parent,orientation,**kwargs):        
+        
+        if orientation == wx.HORIZONTAL:
+            kwargs["style"] = wx.HSCROLL
+        else:
+            kwargs["style"] = wx.VSCROLL
+            
         wx.ScrolledWindow.__init__(self,parent,**kwargs)
-        self.Bind(wx.EVT_SIZE,self.scaleVideos)
-        #self.Bind(wx.EVT_PAINT,self.onPaint)
+        self.Bind(wx.EVT_SIZE,self.onSize)
+        
+        self.brush = wx.TRANSPARENT_BRUSH
+        self.pen = wx.Pen((100,100,100),2)
+        
         self.orientation = orientation
         self.SetSizer(wx.BoxSizer(orientation))
         self.loadVideos([])
         self.zoom = 0.0
+        self.z = 0
+                    
+    def onSize(self,event):
+        print self.z
+        self.z+=1
     
-    def onPaint(self,event):
-        dc = wx.AutoBufferedPaintDC(self)
-        event.Skip()
-                
-    def scaleVideos(self,event=None):
+    def scaleVideos(self):
         if self.videos:
             width,height = self.GetClientSize()
             
@@ -42,12 +52,12 @@ class VideoScrollPanel(wx.ScrolledWindow):
                 
                 video.SetMinSize((w,h))
                 video.SetMaxSize((w,h))
-                
+    
+    def uiUpdate(self,event=None):
+        self.scaleVideos()
         self.Layout()
         self.FitInside()
         self.SetScrollRate(20,20)
-                
-        if event: event.Skip()
     
     def loadVideos(self,filePaths):
         self.videos = []
@@ -66,12 +76,12 @@ class VideoScrollPanel(wx.ScrolledWindow):
             self.totalHeight += size[1]
             
             video = wx.media.MediaCtrl(self,size=size)
-            video.Bind(wx.media.EVT_MEDIA_LOADED, self.scaleVideos)
+            video.Bind(wx.media.EVT_MEDIA_LOADED, self.uiUpdate)
             video.Load(path)
             self.GetSizer().Add(video,0,wx.EXPAND)
             video.SetMinSize((-1,-1))
             self.videos.append(video)
-        self.scaleVideos()
+        self.uiUpdate()
         
 class VideoContainer(wx.Panel):
     def __init__(self,parent,orientation,**kwargs):
@@ -82,7 +92,7 @@ class VideoContainer(wx.Panel):
         
         def onZoom(event):
             self.scrollwindow.zoom = self.zoomControl.GetValue()/1000.0
-            self.scrollwindow.scaleVideos()
+            self.scrollwindow.uiUpdate()
         
         if orientation == wx.HORIZONTAL:            
             self.zoomControl = wx.Slider(self,wx.ID_ANY,0,0,1000,style=wx.SL_VERTICAL|wx.SL_INVERSE)            
